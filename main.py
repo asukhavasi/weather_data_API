904acdaebe43c1573036001c0f4da376a1f36b5c
asukh <sukhavasiabilash@gmail.com>
2025-01-17 21:25:51 -0500

    Update main page to have additional endpoints

--- main.py
+++ main.py
@@ -29,5 +29,27 @@ def api(station,date):
             "date":date,
             "temperature":temp}
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = f"data/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename,skiprows=20,parse_dates=["    DATE"])
+    df_filtered = df[["STAID",' SOUID',"    DATE","   TG"]]
+    # return flask.render_template("home.html",data1=df_filtered.to_html())
+    return df_filtered.to_dict(orient="records")
+
+@app.route("/api/v1/yearly/<station>/<year_input>")
+def station_yearly(station,year_input):
+    filename = f"data/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename,skiprows=20)
+    df['    DATE']=df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year_input))]
+    # return date_temp.to_dict(orient="records")
+    # result = df[df['    DATE'].dt.year == int(year_input)]
+    return result.to_dict(orient="records")
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- templates/test.py
+++ templates/test.py
@@ -1,9 +1,11 @@
 import json
-
+import flask
 import pandas
 
-df = pandas.read_csv("C:/Users/asukh/Documents/Weather_API/data/stations.txt"
-                     "",skiprows=17)
-print(type(df))
+df = pandas.read_csv("C:/Users/asukh/Documents/Weather_API/data"
+                     "/TG_STAID000010.txt"
+                     "",skiprows=20,parse_dates=['    DATE'])
 
+result = df[df['    DATE'].dt.year == 2001]
+print(result.to_dict(orient="records"))
 
